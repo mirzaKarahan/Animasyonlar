b9e9f97bf2e409c45586d3e536751d13c43142e7
mirzaKarahan <mirzaakarahan@gmail.com>
2016-12-24 23:06:28 +0300

    Yanıp Sönen Animasyon
    Yanıp Sönen Alpha animasyonu eklendi

--- app/src/main/java/com/mirza/animasyonlar/MainActivity.java
+++ app/src/main/java/com/mirza/animasyonlar/MainActivity.java
@@ -16,11 +16,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button btnAplhaArttir = (Button) findViewById(R.id.btnAlphaArttir);
         Button btnAplhaAzalt = (Button) findViewById(R.id.btnAlphaAzalt);
+        Button btnBlink = (Button) findViewById(R.id.btnBlink);
         final TextView txtYazi =(TextView) findViewById(R.id.txtYazi);
 
 
         final Animation animAlphaArttir = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.alpha_arttir);
         final Animation animAlphaAzalt = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.alpha_azalt);
+        final Animation animBlink = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.blink);
 
         btnAplhaArttir.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +35,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 txtYazi.startAnimation(animAlphaAzalt);
+            }
+        });
+
+        btnBlink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtYazi.startAnimation(animBlink);
             }
         });
 
