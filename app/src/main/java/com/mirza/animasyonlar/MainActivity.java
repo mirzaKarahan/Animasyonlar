89f7b6fbcdf58c40a3c7fe21741203775accd696
mirzaKarahan <mirzaakarahan@gmail.com>
2016-12-24 22:48:34 +0300

    Button TextView Animation add

--- app/src/main/java/com/mirza/animasyonlar/MainActivity.java
+++ app/src/main/java/com/mirza/animasyonlar/MainActivity.java
@@ -2,6 +2,11 @@ package com.mirza.animasyonlar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnAplhaArttir = (Button) findViewById(R.id.btnAlphaArttir);
+        Button btnAplhaAzalt = (Button) findViewById(R.id.btnAlphaAzalt);
+        final TextView txtYazi =(TextView) findViewById(R.id.txtYazi);
+
+
+        final Animation animAlphaArttir = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.alpha_arttir);
+        final Animation animAlphaAzalt = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.alpha_azalt);
+
+        btnAplhaArttir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtYazi.startAnimation(animAlphaArttir);
+            }
+        });
+
+        btnAplhaAzalt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtYazi.startAnimation(animAlphaAzalt);
+            }
+        });
+
     }
 }
